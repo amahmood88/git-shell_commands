6a9be8f9346665d5d81313bbd7df64b9c5f4fce5
Abusufyan Mahmood <abusufyanmahmood@gmail.com>
2022-10-21 11:29:16 +0000

    msg

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repo to test how to execute git commands using a script.
---
+++ gacp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo git add .
+
+echo "Please enter commit message"
+read msg
+sudo git commit -m 'msg'
+
+sudo git push
+
+# end of file
